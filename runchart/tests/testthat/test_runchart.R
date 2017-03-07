4d7baf6d951832180e0b09e224d2f6bf04135b9d
Joseph Adams <joseph@josephadams.net>
2017-03-07 11:19:05 +0000

    more tests for runchart()

--- runchart/tests/testthat/test_runchart.R
+++ runchart/tests/testthat/test_runchart.R
@@ -5,6 +5,14 @@ context("runchart()")
 
 test_that("shift() handles basic cases", {
   expect_equal(
+    runchart(rep(0,8)),
+    data.frame(base = rep(0,8),
+               base_ext = rep(0,8),
+               shift = rep(NA_real_,8),
+               val = rep(0,8))
+  )
+
+  expect_equal(
     runchart(c(rep(0,8), rep(1,8))),
     data.frame(base = c(rep(0,8), rep(NA,8)),
                base_ext = rep(0,16),
@@ -21,11 +29,99 @@ test_that("shift() handles basic cases", {
     )
 })
 
+# nothing
+# shift
+# sus shift
+
+# combinations:
+# shift then sus
+# sus then shift
+# almost shift
+# delayed shift
+# delayed sus
+# almost sus
+# consecutive sus's
+# consecutive shifts
+
+
 # Corner cases ----------------------------------------------------------------
 
-# test_that("shift() handles corner cases", {
-#
-#   # 2 consecutive shifts same side
+test_that("shift() handles corner cases", {
+
+  # shift then sus
+  expect_equal(
+    runchart(c(rep(0,8), rep(1, 6), rep(-1,9))),
+    data.frame(base = c(rep(0, 8), rep(NA, 6), rep(-1,8), NA),
+               base_ext = c(rep(0, 14), rep(-1, 9)),
+               shift = c(rep(NA, 8), rep(1, 6), rep(NA, 9)),
+               val = c(rep(0,8), rep(1, 6), rep(-1,9)))
+  )
+
+  # sus then shift
+  expect_equal(
+    runchart(c(rep(0,8), rep(1, 9), rep(-1,6))),
+    data.frame(base = c(rep(0, 8), rep(1, 8), rep(NA,7)),
+               base_ext = c(rep(0, 8), rep(1, 15)),
+               shift = c(rep(NA, 17), rep(-1, 6)),
+               val = c(rep(0,8), rep(1, 9), rep(-1,6)))
+  )
+
+  # almost shift
+  expect_equal(
+    runchart(c(rep(0,8), rep(1, 4), 0, NA, 1, 0, NA)),
+    data.frame(base = c(rep(0, 8), rep(NA,9)),
+               base_ext = rep(0, 17),
+               shift = rep(NA_real_, 17),
+               val = c(rep(0,8), rep(1, 4), 0, NA, 1, 0, NA))
+  )
+
+  # delayed shift
+  expect_equal(
+    runchart(c(rep(0,8), rep(1, 4), 0, NA, 1, 0, NA, 1)),
+    data.frame(base = c(rep(0, 8), rep(NA,10)),
+               base_ext = rep(0, 18),
+               shift = c(rep(NA, 8), rep(1, 4), NA, NA, 1, NA, NA, 1),
+               val = c(rep(0,8), rep(1, 4), 0, NA, 1, 0, NA, 1))
+  )
+
+  # delayed sus
+  expect_equal(
+    runchart(c(rep(0,8), rep(1, 7), 0, NA, 1, 0, NA, 1)),
+    data.frame(base = c(rep(0, 8), rep(1,10), rep(NA, 3)),
+               base_ext = c(rep(0, 8), rep(1, 13)),
+               shift = rep(NA_real_, 21),
+               val = c(rep(0,8), rep(1, 7), 0, NA, 1, 0, NA, 1))
+  )
+
+  # almost sus
+  expect_equal(
+    runchart(c(rep(0,8), rep(1, 7), 0, NA, 1, 0, NA)),
+    data.frame(base = c(rep(0, 8), rep(NA, 12)),
+               base_ext = rep(0, 20),
+               shift = c(rep(NA, 8), rep(1, 7), NA, NA, 1, NA, NA),
+               val = c(rep(0,8), rep(1, 7), 0, NA, 1, 0, NA))
+  )
+
+  # consecutive sus
+  expect_equal(
+    runchart(c(rep(0,8), rep(1, 9), rep(-1, 9))),
+    data.frame(base = c(rep(0, 8), rep(1, 8), NA, rep(-1, 8), NA),
+               base_ext = c(rep(0, 8), rep(1, 9), rep(-1, 9)),
+               shift = rep(NA_real_, 26),
+               val = c(rep(0,8), rep(1, 9), rep(-1, 9)))
+  )
+
+  # consecutive shift
+  expect_equal(
+    runchart(c(rep(0,8), rep(1, 7), rep(-1, 6))),
+    data.frame(base = c(rep(0, 8), rep(NA, 13)),
+               base_ext = rep(0, 21),
+               shift = c(rep(NA, 8), rep(1, 7), rep(-1, 6)),
+               val = c(rep(0,8), rep(1, 7), rep(-1, 6)))
+  )
+
+
+# 2 consecutive shifts same side
 #   expect_equal(
 #     shift(
 #       val = c(rep(0,8), rep(1,6), -1, rep(1,6)),
@@ -121,7 +217,7 @@ test_that("shift() handles basic cases", {
 #     c(rep(NA,8), rep(1,6))
 #   )
 #
-# })
+})
 
 # NAs -------------------------------------------------------------------------
 
