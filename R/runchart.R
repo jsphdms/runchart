d01b2f4eb466bdcd6738614264b9490e07918ea9
Joseph Adams <joseph@josephadams.net>
2017-03-22 15:33:54 +0000

    remove shift and sus parameters from runchart()

--- R/runchart.R
+++ R/runchart.R
@@ -1,9 +1,6 @@
 #'Create run chart fields for vector \code{val}.
 #'
-#'
 #'@param val create run chart fields for \code{val}.
-#'@param sus_trigger Number of data points required to create a sustained shift.
-#'@param shift_trigger Number of data points required to create a shift.
 #'@return A data.frame with four columns: 1. The original data. 2. A base line.
 #'  3. An extended base line. 4: Shifts.
 #'@examples
@@ -11,18 +8,14 @@
 #'@seealso \code{\link{sus}} \code{\link{shift}}
 
 #'@export
-runchart <- function(val, sus_trigger = 9, shift_trigger = 6) {
+runchart <- function(val) {
   stopifnot(
     is.numeric(val),
-    is.numeric(sus_trigger),
-    is.numeric(shift_trigger),
-    length(sus_trigger) == 1,
-    length(shift_trigger) == 1,
     length(val) > 0
   )
 
-  rc <- sus(val, trigger = sus_trigger)
-  shift <- shift(val, rc[['base']], rc[['base_ext']], trigger = shift_trigger)
+  rc <- sus(val)
+  shift <- shift(val, rc[['base']], rc[['base_ext']])
 
   rc[['shift']] <- shift
   rc[['val']] <- val
