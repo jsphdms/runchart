0bf7102f9c4fb9bab835d8876145e6f87e767075
Joseph Adams <joseph@josephadams.net>
2017-05-12 15:53:31 +0100

    new split() function. Utility function for plotting.

---
+++ tests/testthat/test_split.R
@@ -0,0 +1,42 @@
+library(firstpackage)
+context("split()")
+
+# Basic functionality ---------------------------------------------------------
+
+test_that("split() handles basic cases", {
+  expect_equal(
+    split(c(1,2,3), 'base'),
+    data.frame(base1 = c(1,NA_real_,NA_real_),
+               base2 = c(NA_real_,2,NA_real_),
+               base3 = c(NA_real_,NA_real_,3))
+  )
+
+  expect_equal(
+    split(c(1,2), 'base'),
+    data.frame(base1 = c(1,NA_real_),
+               base2 = c(NA_real_,2))
+  )
+})
+
+# Corner cases ----------------------------------------------------------------
+
+test_that("split() handles corner cases", {
+
+
+})
+
+# NAs -------------------------------------------------------------------------
+
+test_that("split() handles NAs", {
+
+  expect_equal(
+    split(c(NA,1), 'base'),
+    data.frame(base1 = c(NA_real_,1))
+  )
+
+  expect_equal(
+    split(c(1,NA), 'base'),
+    data.frame(base1 = c(1,NA_real_))
+  )
+
+})
