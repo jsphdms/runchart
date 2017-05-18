4adf48b31dce9c2a70184b6c19ab944c214e4e25
Joseph Adams <joseph@josephadams.net>
2017-05-18 15:29:01 +0100

    plot() now takes a dataframe and is renamed rc_plot(). Output is tidied up.

--- R/plot.R
+++ R/plot.R
@@ -1,36 +1,51 @@
 #'Plot a run chart.
 #'
-#'@param v a vector for \code{plot()} to plot.
+#'@param df a data.frame for \code{rc_plot()} to plot. \code{df} must consist of
+#'exactly two columns: a column of type Date called \code{date} and a column of
+#'numeric type called \code{value}.
 #'@return A plot.
 #'@examples
-#'plot(c(0,1,5,2,3,8,2,2,3,4,7,4,3,4,2,3,1,2,3,2,8,9,7,8,7,9,NA,7,7,8,6,5))
+#'rc_plot(
+#'data.frame(
+#'  date = seq.Date(Sys.Date(), by = "week", length.out = n),
+#'  value = cumsum(rnorm(n))),
+#'"Here is a run chart")
 #'@seealso \code{\link{runchart}} \code{\link{split}}
 #'
 #'@export
-plot <- function(v) {
+rc_plot <- function(df, title = "Default Title") {
   stopifnot(
-    any(!is.na(v)),
-    is.numeric(v),
-    length(v) > 0
+    any(!is.na(df[["value"]])),
+    is.numeric(df[["value"]]),
+    nrow(df) > 0
   )
 
-  rc <- runchart(v)
+  rc <- cbind(df["date"], runchart(df[["value"]]))
+
   base <- split(rc[['base']], 'base')
   base_ext <- split(rc[['base_ext']], 'base_ext')
 
-  p <- ggplot() +
-    geom_line(data = rc, aes(1:nrow(rc), val), colour = 'blue') +
-    geom_point(data = rc, aes(1:nrow(rc), shift), shape = 21, size = 3, colour = 'red',
-               stroke = 2)
+  rc <- cbind(rc, base)
+  rc <- cbind(rc, base_ext)
+
+  p <- ggplot(rc) +
+    geom_line(aes(date, val), colour = 'skyblue', size = 1.1) +
+    geom_point(aes(date, shift), shape = 16, size = 2, colour = 'black',
+               stroke = 2) +
+    labs(title = title)
 
   for (i in names(base)) {
-    p <- p + geom_line(data = base, aes_string(1:nrow(base), i), size = 1.3)
+    p <- p + geom_line(aes_string(rc[["date"]], i), size = 1.3)
   }
 
   for (i in names(base_ext)) {
-    p <- p + geom_line(data = base_ext, aes_string(1:nrow(base_ext), i))
+    p <- p + geom_line(aes_string(rc[["date"]], i))
   }
 
-  p
+  p + theme_classic() + theme(axis.title.x=element_blank(),
+                              axis.title.y=element_blank(),
+                              axis.ticks.x=element_blank(),
+                              axis.ticks.y=element_blank(),
+                              plot.title = element_text(hjust = 0.5))
 
 }
