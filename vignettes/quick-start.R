aad2f039b048a13701f4fc6e6e629a441cb9b4f6
Joseph Adams <jmadams89@gmail.com>
2017-06-05 12:17:36 +0100

    remove older code from vignette.

---
+++ vignettes/quick-start.R
@@ -0,0 +1,8 @@
+## ------------------------------------------------------------------------
+# Please note - this package is in active construction. Once the codebase is 
+# stable, this Quickstart Vignette will include more detail.
+
+## ---- warning = FALSE, fig.width = 7, fig.align = 'center'---------------
+# Please note - this package is in active construction. Once the codebase is 
+# stable, this Quickstart Vignette will include more detail.
+
