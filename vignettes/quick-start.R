121e99d08006ba905cd5caad05c545c38eebadcc
jsphdms <joseph@josephadams.net>
2018-10-28 10:01:21 +0000

    build vignette quick-start

--- vignettes/quick-start.R
+++
@@ -1,8 +0,0 @@
-## ------------------------------------------------------------------------
-# Please note - this package is in active construction. Once the codebase is 
-# stable, this Quickstart Vignette will include more detail.
-
-## ---- warning = FALSE, fig.width = 7, fig.align = 'center'---------------
-# Please note - this package is in active construction. Once the codebase is 
-# stable, this Quickstart Vignette will include more detail.
-
